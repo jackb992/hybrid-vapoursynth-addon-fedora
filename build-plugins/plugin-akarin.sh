eff611ada003ce30a5d2b7897dadfb2d1821a255
jackb992 <jackbiles@outlook.com>
2023-08-07 00:22:01 +0100

    Update plugin-akarin.sh

--- build-plugins/plugin-akarin.sh
+++ build-plugins/plugin-akarin.sh
@@ -1,1 +1,8 @@
-mkgh jackb992/vapoursynth-plugin libakarin
+git clone https://github.com/jackb992/vapoursynth-plugin.git build
+cd build
+meson setup build/ --native-file custom-llvm.ini
+ninja -C build
+
+cd build
+
+cp libakarin.so $VSPREFIX/vsplugins/libakarin.so
