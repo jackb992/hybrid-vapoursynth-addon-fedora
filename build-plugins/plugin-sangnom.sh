8b7e8babb6d5f4309dda238f1c63d3392f33316a
jackb992 <jackbiles@outlook.com>
2023-08-07 13:44:25 +0100

    Update plugin-sangnom.sh

--- build-plugins/plugin-sangnom.sh
+++ build-plugins/plugin-sangnom.sh
@@ -1,7 +1,7 @@
 ghdl dubhater/vapoursynth-sangnom
 chmod +x autogen.sh
 CFLAGS="$CFLAGS -Wno-deprecated-declarations" meson build --prefix="$vsprefix"
-ninja -C build -j $JOBS
-cd "$vsprefix"
-mv "$vsprefix"/lib/libsangnom.so "$vsprefix"/vsplugins/libsangnom.so
+ninja -C build
+cd build
+mv libsangnom.so "$vsprefix"/vsplugins/libsangnom.so
 
