29e15df95115d9cad00b19ae2a60bb1cd4edd82d
jackb992 <jackbiles@outlook.com>
2023-08-07 13:39:48 +0100

    Update plugin-waifu2x-w2xc.sh

--- build-plugins/plugin-waifu2x-w2xc.sh
+++ build-plugins/plugin-waifu2x-w2xc.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/HomeOfVapourSynthEvolution/VapourSynth-Waifu2x-w2xc build
 cd build
-cp -r Waifu2x-w2xc/models $VSPREFIX/lib/vapoursynth
+sudo cp -r Waifu2x-w2xc/models $VSPREFIX/lib/vapoursynth
 meson build
 ninja -C build
 cd build
