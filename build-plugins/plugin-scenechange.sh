f117f0f1734c6add89081eaa0e4775a801c2bf66
jackb992 <jackbiles@outlook.com>
2025-04-22 13:49:12 +0100

    Update plugin-scenechange.sh

--- build-plugins/plugin-scenechange.sh
+++ build-plugins/plugin-scenechange.sh
@@ -1,3 +1,3 @@
-ghdl darealshinji/scenechange
-gcc $CFLAGS $LDFLAGS -shared src/scenechange.c -o libscenechange.so -lm
+ghdl jackb992/scenechange
+gcc -fPIC $CFLAGS $LDFLAGS -shared src/scenechange.c -o libscenechange.so -lm
 finish libscenechange.so
