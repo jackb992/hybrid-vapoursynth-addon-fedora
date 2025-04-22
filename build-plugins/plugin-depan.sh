10c3ad07553269909c1a1d4f3f10e9ec2f392fb2
jackb992 <jackbiles@outlook.com>
2025-04-22 13:30:33 +0100

    Update plugin-depan.sh

--- build-plugins/plugin-depan.sh
+++ build-plugins/plugin-depan.sh
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-wget https://www.dropbox.com/s/d72bougsis2se58/vapoursynth-depan-1.tar.gz
+curl https://www.dropbox.com/s/d72bougsis2se58/vapoursynth-depan-1.tar.gz -L -o vapoursynth-depan-1.tar.gz
 tar xf vapoursynth-depan-1.tar.gz
 cd VapourSynth-DePan-r1
 build libdepan
