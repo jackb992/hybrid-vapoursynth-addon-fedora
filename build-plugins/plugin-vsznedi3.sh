89e5d4612b1bc8d75be3107d3bd3a63f8bd44548
jackb992 <jackbiles@outlook.com>
2025-04-22 16:35:24 +0100

    typo

--- build-plugins/plugin-vsznedi3.sh
+++ build-plugins/plugin-vsznedi3.sh
@@ -1,5 +1,5 @@
 git clone --recursive https://github.com/jackb992/znedi3.git build
 cd build
 make X86=1
-cp vsznedi3.so.so $VSPREFIX/vsplugins/vsznedi3.so
+cp vsznedi3.so $VSPREFIX/vsplugins/vsznedi3.so
 cp nnedi3_weights.bin $VSPREFIX/vsplugins/nnedi3_weights.bin
