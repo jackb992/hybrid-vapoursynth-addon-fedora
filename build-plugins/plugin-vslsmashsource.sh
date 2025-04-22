f4df26bd9b4a265d930835c9f206cfec77c3f2f6
jackb992 <jackbiles@outlook.com>
2025-04-22 16:05:35 +0100

    Update plugin-vslsmashsource.sh

--- build-plugins/plugin-vslsmashsource.sh
+++ build-plugins/plugin-vslsmashsource.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
 
 git clone https://github.com/HomeOfAviSynthPlusEvolution/L-SMASH-Works build
 
