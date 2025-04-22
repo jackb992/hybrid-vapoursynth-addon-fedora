6de56b19a1dc58a8d16f6b5fe295b04d05ca4d10
jackb992 <jackbiles@outlook.com>
2025-04-22 16:09:09 +0100

    Update plugin-vsznedi3.sh

--- build-plugins/plugin-vsznedi3.sh
+++ build-plugins/plugin-vsznedi3.sh
@@ -1,7 +1,5 @@
-#export CC=/usr/bin/gcc-11
-#export CXX=/usr/bin/g++-11
-
-ghc jackb992/znedi3 r2
-git submodule update --init
-build vsznedi3
-install_nnedi3_weights
+git clone --recursive https://github.com/jackb992/znedi3.git build
+cd build
+make X86=1
+cp vsznedi3.so.so $VSPREFIX/vsplugins/vsznedi3.so
+cp nnedi3_weights.bin $VSPREFIX/vsplugins/nnedi3_weights.bin
