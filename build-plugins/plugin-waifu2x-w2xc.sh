64b2b2b1024a71f1ddc3b47f485c52bdd1988443
jackb992 <jackbiles@outlook.com>
2025-04-22 15:34:14 +0100

    Delete build-plugins/plugin-waifu2x-w2xc.sh

--- build-plugins/plugin-waifu2x-w2xc.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/HomeOfVapourSynthEvolution/VapourSynth-Waifu2x-w2xc build
-cd build
-mkdir -p $VSPREFIX/lib/vapoursynth
-cp -r Waifu2x-w2xc/models $VSPREFIX/lib/vapoursynth
-meson build
-ninja -C build
-cd build
-cp libwaifu2x-w2xc.so $VSPREFIX/vsplugins/libwaifu2x-w2xc.so
