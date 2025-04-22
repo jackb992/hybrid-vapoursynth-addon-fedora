6006cab1083c776b418cd857ae7382914c39a10b
jackb992 <jackbiles@outlook.com>
2025-04-22 13:12:33 +0100

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
   sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-  sudo dnf group install "C Development Tools and Libraries" "Development Tools"
+  sudo dnf install @development-tools @c-development
   sudo dnf install  \
     cmake \
     yasm \
